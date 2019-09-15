2ecc3a56854ba4e2ef8710a8d1acf69a976d18b7
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2019-09-15 16:55:11 +0200

    fix: AWS session token is needed with assume-role

--- create-secret.sh
+++ create-secret.sh
@@ -3,4 +3,5 @@ account_id=$1
 credentials=$(aws sts assume-role --role-arn "arn:aws:iam::$account_id:role/terraform-demo" --role-session-name "DemoOPATekton")
 key="$(echo ${credentials} | jq -r '.Credentials.AccessKeyId')"
 secret="$(echo ${credentials} | jq -r '.Credentials.SecretAccessKey')"
-kubectl create secret generic aws-secret  --kubeconfig=$(make env) --from-literal=key=$key --from-literal=secret=$secret > /dev/null 2>&1
+token="$(echo ${credentials} | jq -r '.Credentials.SessionToken')"
+kubectl create secret generic aws-secret  --kubeconfig=$(make env) --from-literal=key=$key --from-literal=secret=$secret --from-literal=token=$token > /dev/null 2>&1
