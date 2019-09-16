3b416b40b14c186fe5348509c431fc31a4d2987d
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2019-09-16 17:36:14 +0200

    fix: "Fix" as I am commenting out the stuff that does not work. Will come back to getting a local docker registry to work later (or switch to Azure)

--- load-policies.sh
+++ load-policies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-kubectl create cm nginx-conf --from-file=nginx.conf=./deployments/registry/conf/nginx.conf --kubeconfig=$(make env)
-kubectl create secret generic nginx-certs --from-file=cert=./deployments/registry/certs/docker-registry.pem --from-file=key=./deployments/registry/certs/docker-registry-key.pem --kubeconfig=$(make env)
+# kubectl create cm nginx-conf --from-file=nginx.conf=./deployments/registry/conf/nginx.conf --kubeconfig=$(make env)
+# kubectl create secret generic nginx-certs --from-file=cert=./deployments/registry/certs/docker-registry.pem --from-file=key=./deployments/registry/certs/docker-registry-key.pem --kubeconfig=$(make env)
 make registry
 kubectl wait  -l run=docker-registry --for=condition=Ready pod
 kubectl port-forward docker-registry 5000:5000 &
